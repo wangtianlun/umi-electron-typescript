d309105748ef4927273a66609708015875c7ee98
wangtianlun <wangtianlun@souche.com>
2019-01-25 16:28:46 +0800

    feat: update index page content

--- src/renderer/pages/index.tsx
+++ src/renderer/pages/index.tsx
@@ -1,5 +1,9 @@
 function Index() {
-  return <div>hello Index Page</div>;
+  return <div style={{ textAlign: "center" }}>
+    <h1>Yay! Welcome to umi and dva!</h1>
+    <br /><br />
+    <img src={require("../assets/yay.jpg")} width="400" />
+  </div>;
 }
 
 export default Index;
