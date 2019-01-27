dc6d37aae76366cce20129a1cb8fd6f1966722fc
wangtianlun <wangtianlun@souche.com>
2019-01-27 13:53:16 +0800

    feat: update index page of title

--- src/renderer/pages/index.tsx
+++ src/renderer/pages/index.tsx
@@ -1,6 +1,6 @@
 function Index() {
   return <div style={{ textAlign: "center" }}>
-    <h1>Yay! Welcome to umi and dva!</h1>
+    <h1>Yay! Welcome to umi-electron-typescript!</h1>
     <br /><br />
     <img src={require("../assets/yay.jpg")} width="400" />
   </div>;
