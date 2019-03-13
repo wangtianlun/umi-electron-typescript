1e333a86b76345f67ff2add7d4cefc973b533506
wangtianlun <wangtianlun@souche.com>
2019-03-13 18:32:50 +0800

    fix: double quota => single quota

--- src/renderer/pages/index.tsx
+++ src/renderer/pages/index.tsx
@@ -4,7 +4,7 @@ function Index() {
       <h1>Yay! Welcome to umi-electron-typescript!</h1>
       <br />
       <br />
-      <img src={require('../assets/yay.jpg')} width="400" />
+      <img src={require('../assets/yay.jpg')} width='400' />
     </div>
   );
 }
